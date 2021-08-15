e95f1822ef345be73caaa4b8733ddd64ec03b094
tawsifhye <tawsif.lucse@gmail.com>
2021-08-15 22:30:27 +0600

    first-commit-pages-created

---
+++ bank.js
@@ -0,0 +1,11 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    
+    if(userEmail == 'abc@gmail.com' && userPassword == 'abc'){
+        window.location.href = 'banking.html';
+    }
+})
